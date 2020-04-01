1b065a4fd36a74da8f9d022e18eb3650b07ccc23
mackeprm <mackeprm@users.noreply.github.com>
2020-04-01 09:57:16 +0200

    * initial import of spring-boot based backend

---
+++ apps/idea-similarity-app/backend/src/main/java/de/fuberlin/innovonto/utils/ideasimilarityappbackend/IdeaSimilarityAppBackendApplication.java
@@ -0,0 +1,13 @@
+package de.fuberlin.innovonto.utils.ideasimilarityappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaSimilarityAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaSimilarityAppBackendApplication.class, args);
+	}
+
+}
---
+++ apps/idea-similarity-app/backend/src/test/java/de/fuberlin/innovonto/utils/ideasimilarityappbackend/IdeaSimilarityAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fuberlin.innovonto.utils.ideasimilarityappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaSimilarityAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
